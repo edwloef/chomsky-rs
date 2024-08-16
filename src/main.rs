0688e5d7b07177774dc5809f9aa54de8cc6498e7
edwloef <edwin.frank.loeffler@gmail.com>
2024-08-17 00:43:34 +0200

    aho-corasick go brrr (-25%)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use aho_corasick::AhoCorasick;
 use clap::Parser;
 use rustc_hash::FxHashSet;
 use serde::Deserialize;
@@ -18,10 +19,36 @@ struct Grammar {
     rules: Vec<Rule>,
 }
 
+#[doc(hidden)]
 #[derive(Debug, Deserialize)]
+struct InterMediateRule {
+    from: String,
+    to: String,
+}
+
+#[derive(Debug)]
 struct Rule {
     from: String,
     to: String,
+
+    aho: AhoCorasick,
+}
+
+impl<'de> Deserialize<'de> for Rule {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let intermediate_rule: InterMediateRule = Deserialize::deserialize(deserializer)?;
+        let aho = AhoCorasick::new([&intermediate_rule.from]).unwrap();
+
+        Ok(Rule {
+            from: intermediate_rule.from,
+            to: intermediate_rule.to,
+
+            aho,
+        })
+    }
 }
 
 fn main() {
@@ -70,11 +97,11 @@ fn is_only_terms(s: &str, grammar: &Grammar) -> bool {
 }
 
 fn apply_rule(words_rule_applied: &mut FxHashSet<String>, rule: &Rule, word: &str) {
-    words_rule_applied.extend(word.match_indices(&rule.from).map(|(i, _)| i).map(|index| {
+    words_rule_applied.extend(rule.aho.find_iter(word).map(|mat| {
         let mut new_string = String::with_capacity(word.len() + rule.to.len() - rule.from.len());
-        new_string.push_str(&word[..index]);
+        new_string.push_str(&word[..mat.start()]);
         new_string.push_str(&rule.to);
-        new_string.push_str(&word[index + rule.from.len()..]);
+        new_string.push_str(&word[mat.end()..]);
         new_string
     }));
 }
