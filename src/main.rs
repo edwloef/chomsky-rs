d7eb411d16e728e31a2352b18f6a66b7a3ff61b2
edwloef <edwin.frank.loeffler@gmail.com>
2024-08-17 10:49:03 +0200

    Multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use aho_corasick::AhoCorasick;
 use clap::Parser;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use rustc_hash::FxHashSet;
 use serde::Deserialize;
-use std::{fs::read_to_string, mem::swap, path::PathBuf};
+use std::{fs::read_to_string, mem::swap, path::PathBuf, sync::Mutex};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -23,7 +24,7 @@ struct Grammar {
 
 #[doc(hidden)]
 #[derive(Debug, Deserialize)]
-struct InterMediateRule {
+struct IntermediateRule {
     from: String,
     to: String,
 }
@@ -41,7 +42,7 @@ impl<'de> Deserialize<'de> for Rule {
     where
         D: serde::Deserializer<'de>,
     {
-        let intermediate_rule: InterMediateRule = Deserialize::deserialize(deserializer)?;
+        let intermediate_rule: IntermediateRule = Deserialize::deserialize(deserializer)?;
         let aho = AhoCorasick::new([&intermediate_rule.from]).unwrap();
 
         Ok(Rule {
@@ -60,34 +61,34 @@ fn main() {
 
     let mut words = FxHashSet::default();
     words.insert(grammar.start_symbol.clone());
-    let mut new_words = FxHashSet::default();
-    let mut words_rule_applied = FxHashSet::default();
-    let mut results = FxHashSet::default();
+    let new_words = Mutex::new(FxHashSet::default());
+    let results = Mutex::new(FxHashSet::<String>::default());
 
     let mut iters = 0;
     while iters < args.max_iters.unwrap_or(usize::MAX) && !words.is_empty() {
-        for word in &words {
-            for rule in &grammar.rules {
+        grammar.rules.par_iter().for_each(|rule| {
+            let mut words_rule_applied = Vec::new();
+            for word in &words {
                 apply_rule(&mut words_rule_applied, rule, word);
+            }
 
-                for new_word in words_rule_applied.drain() {
-                    if is_only_terms(&new_word, &grammar) {
-                        results.insert(new_word);
-                    } else {
-                        new_words.insert(new_word);
-                    }
+            for word in words_rule_applied {
+                if is_only_terms(&word, &grammar) {
+                    results.lock().unwrap().insert(word);
+                } else {
+                    new_words.lock().unwrap().insert(word);
                 }
             }
-        }
+        });
 
-        swap(&mut words, &mut new_words);
-        new_words.clear();
+        words.clear();
+        swap(&mut words, &mut new_words.lock().unwrap());
 
         iters += 1;
     }
 
     dbg!(iters);
-    dbg!(results);
+    dbg!(results.into_inner().unwrap());
 }
 
 fn is_only_terms(s: &str, grammar: &Grammar) -> bool {
@@ -98,7 +99,7 @@ fn is_only_terms(s: &str, grammar: &Grammar) -> bool {
     })
 }
 
-fn apply_rule(words_rule_applied: &mut FxHashSet<String>, rule: &Rule, word: &str) {
+fn apply_rule(words_rule_applied: &mut Vec<String>, rule: &Rule, word: &str) {
     words_rule_applied.extend(rule.aho.find_iter(word).map(|mat| {
         let mut new_string = String::with_capacity(word.len() + rule.to.len() - rule.from.len());
         new_string.push_str(&word[..mat.start()]);
